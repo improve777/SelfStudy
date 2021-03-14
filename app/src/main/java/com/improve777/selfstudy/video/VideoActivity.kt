adceb74d4d23a2cc630142007c681ff22dccc765
improve777 <improve777@gmail.com>
2021-03-15 01:25:52 +0900

    pip 기능 구현

--- app/src/main/java/com/improve777/selfstudy/video/VideoActivity.kt
+++ app/src/main/java/com/improve777/selfstudy/video/VideoActivity.kt
@@ -1,22 +1,34 @@
 package com.improve777.selfstudy.video
 
+import android.app.PictureInPictureParams
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Rational
+import androidx.annotation.RequiresApi
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.improve777.common.base.BindActivity
+import com.improve777.common.utils.click
 import com.improve777.selfstudy.R
 import com.improve777.selfstudy.databinding.ActivityVideoBinding
 import dagger.hilt.android.AndroidEntryPoint
 
+/**
+ * [https://github.com/android/media-samples/blob/main/PictureInPictureKotlin/app/src/main/java/com/example/android/pictureinpicture/MainActivity.kt]
+ */
 @AndroidEntryPoint
 class VideoActivity : BindActivity<ActivityVideoBinding>(R.layout.activity_video) {
 
     private var videoPlayer: ExoPlayer? = null
     private var sampleUrl = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4"
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    private val pictureInPictureParamsBuilder = PictureInPictureParams.Builder()
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -27,6 +39,10 @@ class VideoActivity : BindActivity<ActivityVideoBinding>(R.layout.activity_video
         val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
             .createMediaSource(Uri.parse(sampleUrl))
         videoPlayer?.prepare(mediaSource)
+
+        binding.btnPip click {
+            minimize()
+        }
     }
 
     override fun onResume() {
@@ -39,6 +55,19 @@ class VideoActivity : BindActivity<ActivityVideoBinding>(R.layout.activity_video
         videoPlayer?.playWhenReady = false
         if (isFinishing) {
             videoPlayer?.release()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun minimize() {
+        pictureInPictureParamsBuilder.setAspectRatio(
+            Rational(
+                binding.playerview.width,
+                binding.playerview.height,
+            )
+        )
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            enterPictureInPictureMode(pictureInPictureParamsBuilder.build())
         }
     }
 }
